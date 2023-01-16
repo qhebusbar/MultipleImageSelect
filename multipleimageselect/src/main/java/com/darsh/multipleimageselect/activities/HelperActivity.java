506f3be5b4a4b5e7df1119e1e1cee1cae3e677bf
Jesús Castro <castrojr913@gmail.com>
2023-01-16 18:10:09 -0500

    Update SdkVersion 33

--- multipleimageselect/src/main/java/com/darsh/multipleimageselect/activities/HelperActivity.java
+++ multipleimageselect/src/main/java/com/darsh/multipleimageselect/activities/HelperActivity.java
@@ -4,12 +4,17 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.Settings;
+
 import androidx.annotation.NonNull;
+
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 
 import com.darsh.multipleimageselect.R;
@@ -22,19 +27,23 @@ public class HelperActivity extends AppCompatActivity {
     protected View view;
 
     private final int maxLines = 4;
-    private final String[] permissions = new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+    private String[] getPermissions() {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ?
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE} :
+                new String[]{Manifest.permission.READ_MEDIA_VIDEO, Manifest.permission.READ_MEDIA_IMAGES};
+    }
 
     protected void checkPermission() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_VIDEO) == PackageManager.PERMISSION_GRANTED) {
             permissionGranted();
-
         } else {
-            ActivityCompat.requestPermissions(this, permissions, Constants.PERMISSION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, getPermissions(), Constants.PERMISSION_REQUEST_CODE);
         }
     }
 
-    private void requestPermission() {
+    private void requestOldPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             showRequestPermissionRationale();
 
@@ -43,17 +52,26 @@ public class HelperActivity extends AppCompatActivity {
         }
     }
 
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_MEDIA_IMAGES) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_MEDIA_VIDEO)) {
+            showRequestPermissionRationale();
+        } else {
+            showAppPermissionSettings();
+        }
+    }
+
     private void showRequestPermissionRationale() {
         Snackbar snackbar = Snackbar.make(
-                view,
-                getString(R.string.permission_info),
-                Snackbar.LENGTH_INDEFINITE)
+                        view,
+                        getString(R.string.permission_info),
+                        Snackbar.LENGTH_INDEFINITE)
                 .setAction(getString(R.string.permission_ok), new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         ActivityCompat.requestPermissions(
                                 HelperActivity.this,
-                                permissions,
+                                getPermissions(),
                                 Constants.PERMISSION_REQUEST_CODE);
                     }
                 });
@@ -65,9 +83,9 @@ public class HelperActivity extends AppCompatActivity {
 
     private void showAppPermissionSettings() {
         Snackbar snackbar = Snackbar.make(
-                view,
-                getString(R.string.permission_force),
-                Snackbar.LENGTH_INDEFINITE)
+                        view,
+                        getString(R.string.permission_force),
+                        Snackbar.LENGTH_INDEFINITE)
                 .setAction(getString(R.string.permission_settings), new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -102,14 +120,20 @@ public class HelperActivity extends AppCompatActivity {
         }
     }
 
-    protected void permissionGranted() {}
+    protected void permissionGranted() {
+    }
 
     private void permissionDenied() {
         hideViews();
-        requestPermission();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermission();
+        } else {
+            requestOldPermission();
+        }
     }
 
-    protected void hideViews() {}
+    protected void hideViews() {
+    }
 
     protected void setView(View view) {
         this.view = view;
